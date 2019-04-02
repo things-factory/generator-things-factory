1c54354b6bc2eed484682ed1dd74c7f8722a704d
Stellan Gil <lotstar87@users.noreply.github.com>
2019-04-02 10:58:09 +0900

    app generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,77 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+/*
+ * Copyright © HatioLab Inc. All rights reserved.
+ */
+
+'use strict'
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
+const path = require('path')
+
+function classname(str) {
+  return str
+    .replace(/-/g, ' ')
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+      return letter.toUpperCase()
+    })
+    .replace(/\s+/g, '')
+}
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the divine ${chalk.red('generator-things-factory')} generator!`)
-    );
+      yosay(
+        'Welcome to the smashing ' +
+          chalk.red('generator-things-factory:app') +
+          ' generator!'
+      )
+    )
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: 'input',
+        name: 'appName',
+        message: 'Your things-factory app name?',
+        default: this.appname.replace(/ /g, '-')
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: "What's your Github username",
+        store: true
       }
-    ];
+    ]
 
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+      this.props = props
+    })
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    var tpl = this.props
+
+    this.fs.copyTpl(
+      [
+        this.templatePath() + '/**',
+        this.templatePath() + '/**/.*',
+        '!**/{.DS_Store,_*}/**'
+      ],
+      this.destinationPath(),
+      tpl
+    )
+
+    this.fs.copyTpl(
+      this.templatePath('_gitignore'),
+      this.destinationPath('.gitignore'),
+      tpl
+    )
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      npm: false,
+      bower: false,
+      yarn: true
+    })
   }
-};
+}
