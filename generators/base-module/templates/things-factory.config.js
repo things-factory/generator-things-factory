e8dfd4e7cf313f3478cd032100259e1021b96154
Hearty Oh <heartyoh@hatiolab.com>
2019-05-03 07:54:49 +0800

    module => ui-module, base-module

---
+++ generators/base-module/templates/things-factory.config.js
@@ -0,0 +1,5 @@
+import bootstrap from './src/bootstrap'
+
+export default {
+  bootstrap
+}
---
+++ generators/ui-module/templates/src/bootstrap.js
@@ -0,0 +1,4 @@
+import { store } from '@things-factory/shell'
+import { APPEND_FOOTERBAR, TOOL_POSITION } from '@things-factory/layout-base'
+
+export default function bootstrap() {}
