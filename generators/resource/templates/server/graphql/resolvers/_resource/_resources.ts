56eddcfd18b472f56a0075cbe1773c6bfeec869d
leemyunggyu <jaylee@hatiolab.com>
2019-05-31 13:49:48 +0900

    v0.1.32

--- generators/resource/templates/server/graphql/resolvers/_resource/_resources.ts
+++ generators/resource/templates/server/graphql/resolvers/_resource/_resources.ts
@@ -1,11 +1,11 @@
-import { ListParams, listQueryBuilder } from '@things-factory/shell'
+import { buildQuery, ListParams } from '@things-factory/shell'
 import { getRepository } from 'typeorm'
 import { <%= classifiedResourceName %> } from '../../../entities'
 
 export const <%= pluralCamelCaseResourceName %>Resolver = {
-  async <%= pluralCamelCaseResourceName %>(_: any, params: ListParams, context: any, args: any) {
+  async <%= pluralCamelCaseResourceName %>(_: Object, params: ListParams, context: Object) {
     const queryBuilder = getRepository(<%= classifiedResourceName %>).createQueryBuilder()
-    listQueryBuilder(queryBuilder, params)
+    buildQuery(queryBuilder, params)
     const [items, total] = await queryBuilder.getManyAndCount()
 
     return { items, total }
