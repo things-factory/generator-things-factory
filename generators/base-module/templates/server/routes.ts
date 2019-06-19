ecb96197f0b1d504a5ab58c49caa0b7d2a51aec8
Hearty Oh <heartyoh@hatiolab.com>
2019-06-19 11:01:09 +0900

    v0.1.41

--- generators/base-module/templates/server/routes.ts
+++ generators/base-module/templates/server/routes.ts
@@ -6,12 +6,6 @@ process.on('bootstrap-module-history-fallback' as any, (app, fallbackOption) => 
    */
 })
 
-process.on('bootstrap-module-middleware' as any, app => {
-  /*
-   * koa application에 service를 추가할 수 있다.
-   */
-})
-
 process.on('bootstrap-module-route' as any, (app, routes) => {
   /*
    * koa application에 routes 를 추가할 수 있다.
