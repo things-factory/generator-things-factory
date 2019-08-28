32d23c0edcaa48f7c2a24cea61007e95902a223b
Hearty Oh <heartyoh@hatiolab.com>
2019-08-28 21:43:03 +0900

    v0.1.70

--- generators/resource/templates/server/graphql/resolvers/_resource/_update-resource.ts
+++ generators/resource/templates/server/graphql/resolvers/_resource/_update-resource.ts
@@ -2,9 +2,11 @@ import { getRepository } from 'typeorm'
 import { <%= classifiedResourceName %> } from '../../../entities'
 
 export const update<%= classifiedResourceName %> = {
-  async update<%= classifiedResourceName %>(_: any, { name, patch }) {
+  async update<%= classifiedResourceName %>(_: any, { name, patch }, context: any) {
     const repository = getRepository(<%= classifiedResourceName %>)
-    const <%= camelCaseResourceName %> = await repository.findOne({ name })
+    const <%= camelCaseResourceName %> = await repository.findOne({ 
+      where: { domain: context.domain, name }
+    })
 
     return await repository.save({
       ...<%= camelCaseResourceName %>,
