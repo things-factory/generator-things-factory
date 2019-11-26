c7f5e14da4b96e3e0d0ba6d8ccb807c4332038f1
Hearty Oh <heartyoh@hatiolab.com>
2019-11-26 20:10:49 +0800

    Update route.js

--- generators/app/templates/client/route.js
+++ generators/app/templates/client/route.js
@@ -1,5 +1,8 @@
 export default function route(page) {
   switch (page) {
+    case '':
+      return '/<%= moduleName %>-main'
+
     case '<%= moduleName %>-main':
       import('./pages/main')
       return page
