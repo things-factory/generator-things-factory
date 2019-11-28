3bf9d31528961361571888f4b7617d239e57a4ee
Hearty Oh <heartyoh@hatiolab.com>
2019-11-28 14:09:51 +0800

    app server configuration

--- generators/app/templates/config.development.js
+++ generators/app/templates/config.development.js
@@ -3,5 +3,25 @@ module.exports = {
   uploads: 'uploads',
   attachmentsPath: 'attachments',
   SECRET: '0xD58F835B69D207A76CC5F84a70a1D0d4C79dAC95',
+  logger: {
+    file: {
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: false,
+      maxSize: '20m',
+      maxFiles: '2d',
+      level: 'info'
+    },
+    console: {
+      level: 'silly'
+    }
+  },
+  ormconfig: {
+    name: 'default',
+    type: 'sqlite',
+    database: 'db.sqlite',
+    synchronize: true,
+    logging: true
+  },
   googleFontAPIKey: ''
 }
--- generators/app/templates/config.production.js
+++ generators/app/templates/config.production.js
@@ -3,5 +3,22 @@ module.exports = {
   uploads: 'uploads',
   attachmentsPath: 'attachments',
   SECRET: '0xD58F835B69D207A76CC5F84a70a1D0d4C79dAC95',
+  logger: {
+    file: {
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '200m',
+      maxFiles: '1m',
+      level: 'info'
+    }
+  },
+  ormconfig: {
+    name: 'default',
+    type: 'sqlite',
+    database: 'db.sqlite',
+    synchronize: true,
+    logging: true
+  },
   googleFontAPIKey: ''
 }
