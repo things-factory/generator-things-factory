57a15992574db0f74d784fd027c789db13dd7b93
Hearty Oh <heartyoh@hatiolab.com>
2019-12-04 22:00:24 +0800

    add storage default configuration

--- generators/app/templates/config.development.js
+++ generators/app/templates/config.development.js
@@ -1,7 +1,6 @@
 module.exports = {
   port: 3000,
   uploads: 'uploads',
-  attachmentsPath: 'attachments',
   SECRET: '0xD58F835B69D207A76CC5F84a70a1D0d4C79dAC95',
   logger: {
     file: {
@@ -15,6 +14,17 @@ module.exports = {
     console: {
       level: 'silly'
     }
+  },
+  storage: {
+    /* storage configuration for file-system */
+    type: 'file',
+    base: 'attachments'
+    /* storage configuration for s3-bucket
+      type: 's3',
+      accessKeyId: 'YOUR ACCESS KEY ID',
+      secretAccessKey: 'YOUR SECRET ACCESS KEY',
+      bucketName: 'YOUR S3 BUCKET NAME'
+    */
   },
   ormconfig: {
     name: 'default',
--- generators/app/templates/config.production.js
+++ generators/app/templates/config.production.js
@@ -1,7 +1,6 @@
 module.exports = {
   port: 3000,
   uploads: 'uploads',
-  attachmentsPath: 'attachments',
   SECRET: '0xD58F835B69D207A76CC5F84a70a1D0d4C79dAC95',
   logger: {
     file: {
@@ -12,6 +11,17 @@ module.exports = {
       maxFiles: '1m',
       level: 'info'
     }
+  },
+  storage: {
+    /* storage configuration for file-system */
+    type: 'file',
+    base: 'attachments'
+    /* storage configuration for s3-bucket
+      type: 's3',
+      accessKeyId: 'YOUR ACCESS KEY ID',
+      secretAccessKey: 'YOUR SECRET ACCESS KEY',
+      bucketName: 'YOUR S3 BUCKET NAME'
+    */
   },
   ormconfig: {
     name: 'default',
